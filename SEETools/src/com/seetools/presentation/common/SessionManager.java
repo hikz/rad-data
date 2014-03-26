7a9066ed6b80ccfce74034c744c6379849bda16b
hikz <ramz.prasadz@gmail.com>
2014-03-26 21:58:50 +0530

    clean up

---
+++ SEETools/src/com/seetools/presentation/common/SessionManager.java
@@ -0,0 +1,29 @@
+package com.seetools.presentation.common;
+
+import java.util.Map;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+public class SessionManager {
+
+	
+	public static void addSessionAttribute(String key, Object value) {
+		
+		getSessionMap().put(key, value);
+		
+	}
+	
+	public static Object getSessionAttribute(String key){
+		return getSessionMap().get(key);
+	}
+	
+	private static Map<String, Object> getSessionMap(){
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		return externalContext.getSessionMap();
+	}
+	
+	public static void logout(){
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+	}
+}
